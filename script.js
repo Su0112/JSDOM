db51d51ca86d666641304f12c9d90cd154aff6c1
Su Shruthi <shruthacm@gmail.com>
2023-02-10 23:08:04 -0800

    commit

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+// Handing Click Events
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
